9a8db0e397055318d3e40a2bcb77f9553b8166e3
jianhuang <jianhuang@upchina.com>
2019-04-12 17:59:28 +0800

    app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-// app.use('/static', express.static(path.join(__dirname, './public')));
+app.use('/static', express.static(path.join(__dirname, './public')));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
